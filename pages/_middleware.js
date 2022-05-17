98840147cc928ec194f64eb75f817899619faa34
p2426 <phoenixmee91@gmail.com>
2022-05-17 17:01:24 +0100

    Delete _middleware.js

--- pages/_middleware.js
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from 'next/server';
-
-export function middleware(request) {
-  const country = request.geo.country || 'US';
-
-  return new Response(`Hello, World. I'm in ${country}`);
-}
