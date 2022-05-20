17b241d880902226cd710396cbf8551320b6affa
Phoenix Mee <phoenixmee91@gmail.com>
2022-05-20 09:31:25 +0100

    Cache Serverless function

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
+    res.status(200).json({ name: 'John Doe' })
 }
--- pages/profile/_middleware.js
+++ pages/profile/_middleware.js
@@ -22,8 +22,6 @@ export function middleware(request, event) {
         redirect.cookie(key, values[i]);
     });
 
-    // Avoid infinite loop by only redirecting if the query
-    // params were changed
     if (changed) {
         return redirect;
     }
